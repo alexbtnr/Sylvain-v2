4149386424fd9bb47b47450968cb38a6820edd3f
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-06-28 23:44:16 +0300

    Animated and styled the navbar and also adjusted for mobile viewing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,8 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
+import Hamburger from "hamburger-react";
 
 const Nav = ({ hrefList, textList }) => {
   const location = useLocation();
@@ -32,6 +33,43 @@ const Nav = ({ hrefList, textList }) => {
                 <a href={hrefList[5]}>{textList[5]}</a>
               </li>
             </ul>
+          </div>
+          <div className='mobile-nav'>
+            <AnimatePresence>
+              {isOpen && (
+                <motion.ul
+                  initial={{ opacity: 0, y: -500 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -500 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <li onClick={() => setIsOpen(false)}>
+                    <a href='#showcase'>Home</a>
+                  </li>
+                  <li onClick={() => setIsOpen(false)}>
+                    <a href={hrefList[0]}>{textList[0]}</a>
+                  </li>
+                  <li onClick={() => setIsOpen(false)}>
+                    <a href={hrefList[1]}>{textList[1]}</a>
+                  </li>
+                  <li onClick={() => setIsOpen(false)}>
+                    <a href={hrefList[2]}>{textList[2]}</a>
+                  </li>
+                  <li onClick={() => setIsOpen(false)}>
+                    <a href={hrefList[3]}>{textList[3]}</a>
+                  </li>
+                  <li onClick={() => setIsOpen(false)}>
+                    <a href={hrefList[4]}>{textList[4]}</a>
+                  </li>
+                  <li onClick={() => setIsOpen(false)}>
+                    <a href={hrefList[5]}>{textList[5]}</a>
+                  </li>
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          </div>
+          <div className='hamburger-wrapper' id='wrapper'>
+            <Hamburger toggled={isOpen} toggle={setIsOpen} />
           </div>
         </div>
       )}
@@ -65,6 +103,69 @@ const StyledNav = styled(motion.nav)`
     }
     li:hover:after {
       opacity: 1;
+    }
+  }
+
+  div.mobile-nav,
+  .hamburger-react {
+    display: none;
+  }
+
+  @media (max-width: 800px) {
+    div.normal-nav {
+      display: none;
+    }
+
+    div.mobile-nav,
+    .hamburger-react {
+      display: block;
+    }
+
+    div.mobile-nav {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      z-index: 10;
+
+      ul {
+        background: rgba(0, 0, 0, 0.7);
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        flex-direction: column;
+        text-align: left;
+        justify-content: flex-start;
+        align-items: flex-start;
+        padding-top: 12vh;
+
+        li {
+          margin: 0;
+          margin-bottom: 0.5rem;
+          padding-left: 5%;
+          width: 100%;
+
+          a {
+            color: #fff;
+          }
+        }
+        li:after {
+          border-bottom: 0;
+        }
+        li:first-child {
+          border-top: 2px solid #fff;
+        }
+      }
+    }
+    .hamburger-wrapper {
+      border-radius: 50%;
+      background: rgba(0, 0, 0, 0.5);
+      color: #fff;
+      z-index: 10;
+      position: fixed;
+      top: 3%;
+      right: 3%;
     }
   }
 `;
