2da17a8d6f4666bf9d76e6af59da923a037965ee
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-06-29 18:30:25 +0300

    Carousel created and styled for both lg screens and mobile

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,155 @@
 import React from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+import CarouselItem from "./CarouselItem";
+
+import cabinDay from "../img/cabinDay.jpg";
+import cabinNight from "../img/cabinNight.jpg";
+import cabinCity from "../img/cabinCity.jpg";
+import cabinInterior1 from "../img/cabinInterior1.jpg";
+import cabinInterior2 from "../img/cabinInterior2.jpg";
+import cabinInterior3 from "../img/cabinInterior3.jpg";
+
+const images = [
+  {
+    id: 1,
+    imageName: cabinDay,
+    primaryTitle: "Accomodation",
+    imageId: "cabinDay",
+    secondaryTitle: "Lorem ipsum dolor sit amet.",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eos dolorem quae exercitationem non assumenda?",
+  },
+  {
+    id: 2,
+    imageName: cabinNight,
+    primaryTitle: "Accomodation",
+    imageId: "cabinNight",
+    secondaryTitle: "Lorem ipsum dolor sit amet.",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eos dolorem quae exercitationem non assumenda?",
+  },
+  {
+    id: 3,
+    imageName: cabinCity,
+    primaryTitle: "Accomodation",
+    imageId: "cabinCity",
+    secondaryTitle: "Lorem ipsum dolor sit amet.",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eos dolorem quae exercitationem non assumenda?",
+  },
+  {
+    id: 4,
+    imageName: cabinInterior1,
+    primaryTitle: "Accomodation",
+    imageId: "cabinInterior1",
+    secondaryTitle: "Lorem ipsum dolor sit amet.",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eos dolorem quae exercitationem non assumenda?",
+  },
+  {
+    id: 5,
+    imageName: cabinInterior2,
+    primaryTitle: "Accomodation",
+    imageId: "cabinInterior2",
+    secondaryTitle: "Lorem ipsum dolor sit amet.",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eos dolorem quae exercitationem non assumenda?",
+  },
+  {
+    id: 6,
+    imageName: cabinInterior3,
+    primaryTitle: "Accomodation",
+    imageId: "cabinInterior3",
+    secondaryTitle: "Lorem ipsum dolor sit amet.",
+    paragraph:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eos dolorem quae exercitationem non assumenda?",
+  },
+];
 
 const Carousel = () => {
-  return <div>carousel lmao</div>;
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <StyledCarousel id='carousel'>
+      <div className='container'>
+        <div className='slider-container'>
+          <Slider {...settings}>
+            {images.map((image) => (
+              <CarouselItem
+                key={image.id}
+                img={image.imageName}
+                id={image.imageId}
+                primaryTitle={image.primaryTitle}
+                secondaryTitle={image.secondaryTitle}
+                paragraph={image.paragraph}
+              />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </StyledCarousel>
+  );
 };
+
+const StyledCarousel = styled.section`
+  .container {
+    padding-bottom: 2rem;
+
+    .slider-container {
+      text-align: center;
+
+      .slick-slider {
+        max-width: 97%;
+        margin: auto;
+
+        button.slick-arrow::before {
+          color: #000;
+          opacity: 0.5;
+          transition: all 0.75s ease;
+        }
+        button.slick-arrow:hover::before,
+        button.slick-arrow:hover {
+          opacity: 1;
+        }
+
+        .slick-dots {
+          li {
+            button::before {
+              font-size: 0.75rem;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 800px) {
+    .container {
+      .slider-container {
+        .slick-slider {
+          max-width: 75%;
+
+          .slick-next:before,
+          .slick-prev:before {
+            font-size: 45px;
+          }
+
+          .slick-prev {
+            left: -50px;
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Carousel;
