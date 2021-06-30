f396693dde939b67366b068361460f22421c5bb9
Alex Butnaru <alex.butnaru1102@gmail.com>
2021-06-30 03:41:15 +0300

    Created the main reservations component

---
+++ src/components/LandingReservations.js
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
+
+const LandingReservations = () => {
+  return <StyledLanding>reservations</StyledLanding>;
+};
+
+const StyledLanding = styled(motion.div)`
+  height: 100vh;
+`;
+
+export default LandingReservations;
--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Contact from "../components/Contact";
+import GoToTopButton from "../components/GoToTopButton";
+import LandingReservations from "../components/LandingReservations";
 
-const Reservations = () => {
-  return <div>reservations lmao</div>;
+const Reservations = ({ formList, setFormList, alert, setAlert }) => {
+  return (
+    <div>
+      <LandingReservations
+        formList={formList}
+        setFormList={setFormList}
+        alert={alert}
+        setAlert={setAlert}
+      />
+      <Contact />
+      <GoToTopButton />
+    </div>
+  );
 };
 
 export default Reservations;
